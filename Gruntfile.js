e0446e9b3363fd35d5819633e608d8b4b974d44b
Trond Fröding Hatlen <trondfroding@gmail.com>
2015-04-09 14:01:39 +0200

    added grunt task for scaling down pizza image

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+/*
+ After you have changed any settings for the responsive_images task,
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 100,
+            height: 62,
+            quality: 70,
+            name: '@1x',
+            aspectRatio: false
+          }, {
+            width: 200,
+            height: 124,
+            quality: 70,
+            name: '@2x',
+            aspectRatio: false
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['pizzeria.jpg'],
+          cwd: 'img_src/',
+          dest: 'img/'
+        }]
+      }
+    },
+    // not in use yet
+    responsive_views_images:{
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 100,
+            quality: 30,
+            name: '@1x'
+          }, {
+            width: 200,
+            quality: 30,
+            name: '@2x'
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'views/images_src/',
+          dest: 'views/images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['img'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['img']
+        },
+      },
+    },
+    // copy images that shouldn't be cropped
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: ['img_src/*.{jpg,png}'],
+          dest: 'img/',
+          flatten: true
+        }]
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('make-images', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
+  grunt.registerTask('default', ['make-images']);
+};
